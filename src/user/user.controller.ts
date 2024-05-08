37ebd3b372fd2b8f72618f4ec69bbb74e5eafae9
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-08 19:10:50 -0300

    feat(back-end): add use pipes validate

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserInputDto } from './dtos/createUserInput.dto';
 import { CreateUserService } from './user.service';
 
 @Controller('user')
 export class createUserController {
     constructor(private readonly createUserService: CreateUserService) { }
+
+    @UsePipes(ValidationPipe)
     @Post()
     async createUser(@Body() createUser: CreateUserInputDto) {
         return this.createUserService.execute(createUser)
