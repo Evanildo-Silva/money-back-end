4a15ccb174d9053490fd0b03281f716c38bba70a
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-08 18:56:30 -0300

    refactor(back-end): transform interface into class and add swagger properties and class-validator validations

--- src/user/dtos/createUserInput.dto.ts
+++ src/user/dtos/createUserInput.dto.ts
@@ -1,5 +1,18 @@
-export interface CreateUserInputDto {
-    name: string,
-    email: string,
-    password: string,
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserInputDto {
+  @ApiProperty({ description: 'Nome do usuário', required: true })
+  @IsNotEmpty({ message: 'Forneça um nome para o usuário' })
+  name: string;
+
+  @ApiProperty({ description: 'Email do usuário', required: true })
+  @IsEmail({}, { message: 'Formato de email inválido' })
+  @IsNotEmpty({ message: 'Forneça um email válido' })
+  email: string;
+
+  @ApiProperty({ description: 'Senha do usuário', required: true, minLength: 6 })
+  @IsNotEmpty({ message: 'Forneça uma senha' })
+  @MinLength(6, { message: 'A senha deve ter pelo menos 6 caracteres' })
+  password: string;
 }
