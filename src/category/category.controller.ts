14253ca7eb502bdf10d0491f71b5d991e01e49e2
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-21 19:45:38 -0300

    feat(authentication): api tags

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CategoryService } from './category.service';
 
+@ApiTags('Categories')
 @Controller('category')
 export class CategoryController {
     constructor(private readonly categoryService: CategoryService) { }
