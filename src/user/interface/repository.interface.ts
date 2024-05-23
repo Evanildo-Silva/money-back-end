51e2456be7262ededc78f393c82ab93c66e074a6
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-23 19:57:30 -0300

    refactor: replace name

--- src/user/interface/repository.interface.ts
+++ src/user/interface/repository.interface.ts
@@ -2,6 +2,6 @@ import { CreateUserInputDto } from "../dtos/createUserInput.dto";
 import { UserEntity } from "../entities/user.entity";
 
 export interface IUsersRepository {
-    create(data: CreateUserInputDto): Promise<UserEntity>;
+    create(user: CreateUserInputDto): Promise<UserEntity>;
     findByEmail(email: string): Promise<UserEntity | null>;
 }
