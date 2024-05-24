25a0eb2c21bada4621f9782df9caa6c9cdef9a00
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-23 21:57:17 -0300

    refactor(expenses): adds findExpenseByDescriptionService

--- src/expenses/controller/expenses.controller.ts
+++ src/expenses/controller/expenses.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateExpenseInputDto } from 'src/expenses/services/createExpense/dto/createExpense.dto';
 import { CreateExpensesService } from 'src/expenses/services/createExpense/services/createExpenses.service';
 import { ExpenseEntity } from '../entity/expenses.entity';
+import { FindExpenseByDescriptionInputDto } from '../services/findExpenseByName/dto/findExpenseByDescription.dto';
+import { FindExpenseByDescriptionService } from '../services/findExpenseByName/services/findExpenseByDescription.service';
 
 @ApiTags('Expenses')
 @Controller('expenses')
 export class ExpensesController {
     constructor(
-        private readonly createExpenseService: CreateExpensesService
+        private readonly createExpenseService: CreateExpensesService,
+        private readonly findExpenseByDescriptionService: FindExpenseByDescriptionService,
     ) { }
 
     @UsePipes(ValidationPipe)
@@ -18,4 +21,13 @@ export class ExpensesController {
 
         return newExpense
     }
+
+    @UsePipes(ValidationPipe)
+    @Get('/find-by-description')
+    async findExpenseByDescription(@Query() { description }: FindExpenseByDescriptionInputDto): Promise<ExpenseEntity> {
+        const expense = await this.findExpenseByDescriptionService.execute({ description })
+
+        return expense
+    }
+
 }
