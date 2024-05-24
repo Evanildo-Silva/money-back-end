d1ba41349f9ae2a2e15b34c44eb5df6740b3dd81
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-23 21:59:12 -0300

    refactor(expenses): adds FindExpenseByDescriptionService and FindExpenseByDescriptionRepository

--- src/expenses/services/expensesServices.module.ts
+++ src/expenses/services/expensesServices.module.ts
@@ -4,10 +4,17 @@ import { ExpensesController } from "../controller/expenses.controller";
 import { ExpenseEntity } from "../entity/expenses.entity";
 import { CreateExpensesRepository } from "./createExpense/repository/createExpenses.repository";
 import { CreateExpensesService } from "./createExpense/services/createExpenses.service";
+import { FindExpenseByDescriptionRepository } from "./findExpenseByName/repository/findExpenseByDescription.repository";
+import { FindExpenseByDescriptionService } from "./findExpenseByName/services/findExpenseByDescription.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([ExpenseEntity])],
     controllers: [ExpensesController],
-    providers: [CreateExpensesService, CreateExpensesRepository],
+    providers: [
+        CreateExpensesService,
+        FindExpenseByDescriptionService,
+        CreateExpensesRepository,
+        FindExpenseByDescriptionRepository
+    ],
 })
 export class ExpensesServicesModule { }
