77956fff943a1f2cb3dde267e8d0c050c7e49e73
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-23 21:29:59 -0300

    feat(expenses): create the findExpenseByDescription service

---
+++ src/expenses/services/findExpenseByName/services/findExpenseByDescription.service.ts
@@ -0,0 +1,23 @@
+import { HttpException, Injectable } from "@nestjs/common";
+import { FindExpenseByDescriptionInputDto } from "../dto/findExpenseByDescription.dto";
+import { FindExpenseByDescriptionRepository } from "../repository/findExpenseByDescription.repository";
+
+@Injectable()
+export class FindExpenseByDescription {
+    constructor(
+        private readonly findExpenseByDescriptionRepository: FindExpenseByDescriptionRepository
+    ) { }
+
+    public async execute({ description }: FindExpenseByDescriptionInputDto) {
+        const expense = await this.findExpenseByDescriptionRepository.findExpenseByDescription({ description })
+
+        if (!expense) {
+            throw new HttpException(
+                'Erro ao buscar pedidos de exames do usuário',
+                500,
+            );
+        }
+
+        return expense
+    }
+}
