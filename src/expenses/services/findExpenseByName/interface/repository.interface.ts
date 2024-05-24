ad01c4b85a4fe55f8323632f27a321a0c7ab752c
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-23 21:29:38 -0300

    refactor(expenses): add dto

--- src/expenses/services/findExpenseByName/interface/repository.interface.ts
+++ src/expenses/services/findExpenseByName/interface/repository.interface.ts
@@ -1,5 +1,6 @@
 import { ExpenseEntity } from "src/expenses/entity/expenses.entity";
+import { FindExpenseByDescriptionInputDto } from "../dto/findExpenseByDescription.dto";
 
 export interface IFindExpenseByDescriptionRepository {
-    findExpenseByDescription(description: string): Promise<ExpenseEntity | null>;
+    findExpenseByDescription({ description }: FindExpenseByDescriptionInputDto): Promise<ExpenseEntity | null>;
 }
--- src/expenses/services/findExpenseByName/repository/findExpenseByDescription.repository.ts
+++ src/expenses/services/findExpenseByName/repository/findExpenseByDescription.repository.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { ExpenseEntity } from "src/expenses/entity/expenses.entity";
 import { Repository } from "typeorm";
+import { FindExpenseByDescriptionInputDto } from "../dto/findExpenseByDescription.dto";
 import { IFindExpenseByDescriptionRepository } from "../interface/repository.interface";
 
 @Injectable()
 export class FindExpenseByDescriptionRepository implements IFindExpenseByDescriptionRepository {
-    constructor(private readonly expenseRepository: Repository<ExpenseEntity>) { }
-    public async findExpenseByDescription(description: string): Promise<ExpenseEntity> {
+    constructor(
+        private readonly expenseRepository: Repository<ExpenseEntity>
+    ) { }
+
+    public async findExpenseByDescription({ description }: FindExpenseByDescriptionInputDto): Promise<ExpenseEntity> {
         const expense = await this.expenseRepository.findOneBy({ description });
 
         return expense;
