bd0284dca4e7240fa0f57a9554a95e8feec34c79
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-06-05 20:08:08 -0300

    refactor(expenses): add repository injection

--- src/expenses/services/listExpenses/services/listExpenses.service.ts
+++ src/expenses/services/listExpenses/services/listExpenses.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { ExpenseEntity } from "src/expenses/entity/expenses.entity";
+import { ListExpensesRepository } from "../repository/listExpenses.repository";
 
 @Injectable()
 export class ListExpensesService {
-    constructor() { }
-    public async execute() { }
+    constructor(
+        private readonly listExpensesRepository: ListExpensesRepository
+    ) { }
+
+    public async execute(): Promise<ExpenseEntity[]> {
+        try {
+            const listExpenses = await this.listExpensesRepository.listAllExpenses()
+
+            return listExpenses
+        } catch (error) {
+            throw new HttpException(
+                'Ocorreu um erro ao listar as despesas.',
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
 }
